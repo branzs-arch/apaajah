f562afb19836fc0b606e958cc7056c7f65d4880c
branzs-arch <zibranalwi50@gmail.com>
2025-09-04 14:48:39 +0700

    first commit laravel project

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,10 +32,7 @@
                         <li><a class="dropdown-item" href="#!">Settings</a></li>
                         <li><a class="dropdown-item" href="#!">Activity Log</a></li>
                         <li><hr class="dropdown-divider" /></li>
-                        <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <button type="submit" class="cyber-button">Logout</button>
-    </form>
+                        <li><a class="dropdown-item" href="#!">Logout</a></li>
                     </ul>
                 </li>
             </ul>
